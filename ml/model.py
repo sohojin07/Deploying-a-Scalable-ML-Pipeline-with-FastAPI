22d0dd379b4b00061ea0b5cdaca4a280b79514c5
sohojin07 <asm3387@wgu.edu>
2026-02-14 12:39:20 -0700

    Complete code in model.py

--- ml/model.py
+++ ml/model.py
@@ -1,9 +1,13 @@
 import pickle
-from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.metrics import fbeta_score, precision_score, recall_score, f1_score, accuracy_score
 from ml.data import process_data
-# TODO: add necessary import
+from ml.model_metrics import compute_model_metrics
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import os
 
 # Optional: implement hyperparameter tuning.
+
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -19,8 +23,13 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42,
+        n_jobs=-1
+    )
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +68,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    return model.predict(X)
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +80,17 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    #Make sure folder exists
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    #Save the model
+    joblib.dump(model, path)
+    print(f"model saved to {path}")
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    model = joblib.load(path)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -117,12 +129,22 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    # Slice data
+    data_slice = data[data[column_name] == slice_value]
+
+    if data_slice.shape[0] == 0:
+        print(f"No rows for {column_name}={slice_value}")
+        return None, None, None
+    
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data_slice,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
